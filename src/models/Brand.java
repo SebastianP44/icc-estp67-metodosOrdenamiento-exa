5f4c48b676998344d3da49274a2e78a206e5ea91
Your Name <you@example.com>
2026-05-20 00:55:59 +0000

    Error en el metodo yearisvalid

--- src/models/Brand.java
+++ src/models/Brand.java
@@ -1,5 +1,53 @@
 package models;
 
-public class Brand {
+import java.util.Arrays;
 
+public class Brand {
+  private String brandName;
+  private CarModel[] models;
+  
+  public Brand(String brandName, CarModel[] models) {
+    this.brandName = brandName;
+    this.models = models;
+  }
+  
+  public Brand() {
+  }
+
+  public String getBrandName() {
+    return brandName;
+  }
+
+  public void setBrandName(String brandName) {
+    this.brandName = brandName;
+  }
+
+  public CarModel[] getModels() {
+    return models;
+  }
+
+  public void setModels(CarModel[] models) {
+    this.models = models;
+  }
+
+   public int getTotalValidYears() {
+        int total = 0;
+        for (CarModel model : models) {
+            for (CarYear carYear : model.getYears()) {
+                if (carYear.isValid()) {
+                    total++;
+                }
+            }
+        }
+        return total;
+      }
+
+ @Override
+  public String toString() {
+    return "Brand [brandName=" + brandName + ", models=" + Arrays.toString(models) + "]";
+  }
 }
+
+
+
+
